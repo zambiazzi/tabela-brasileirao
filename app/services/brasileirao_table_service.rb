daec0c070a6c14a6d75f27ae9fffc2f97b91fc90
lucas zambiazzi <lucaszambiazzi194@gmail.com>
2024-11-18 13:37:31 -0300

    structuring data in an array at services

--- app/services/brasileirao_table_service.rb
+++ app/services/brasileirao_table_service.rb
@@ -8,18 +8,38 @@ class BrasileiraoTableService
     Authorization: "Bearer #{Figaro.env.API_KEY}"
   }
 
+  def self.go 
+    res = self.request
+
+    teams = []
+
+    res.each do |res|
+      teams.push({
+          posicao: res['posicao'],
+          escudo: res['time']['escudo'],
+          nome: res['time']['nome_popular'],
+          pontos: res['pontos'],
+          jogos: res['jogos'],
+          vitorias: res['vitorias'],
+          empates: res['empates'],
+          derrotas: res['derrotas'],
+          saldo_de_gols: res['saldo_gols'],
+          ultimos_jogos: res['ultimos_jogos'],
+       }) 
+    end
+
+    return teams if teams.present?
+
+    raise "Time não encontrado." unless res.present?
+
+  rescue Exception => e
+    raise e.message
+  end
+
   private 
 
-  def request
+  def self.request
     res = RestClient.get(TABLE_URL_DATA, @headers)
-    puts table_data.code
-    puts JSON.parse(table_data.body)
+    JSON.parse(res.body)
   end
 end
-
-
-# {"posicao"=>1, "pontos"=>68, 
-# "time"=>{"time_id"=>22, "nome_popular"=>"Botafogo", "sigla"=>"BOT", 
-# "escudo"=>"https://cdn.api-futebol.com.br/escudos/638d349052558.svg"}, 
-# "jogos"=>33, "vitorias"=>20, "empates"=>8, "derrotas"=>5, "gols_pro"=>52, "gols_contra"=>26, "saldo_gols"=>26, 
-# "aproveitamento"=>68, "variacao_posicao"=>0, "ultimos_jogos"=>["v", "e", "v", "v", "e"]}
